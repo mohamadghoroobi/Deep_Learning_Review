6d2abbec899da20f920f8b283f6fef9bb22a67c8
mohamadghoroobi <m.ghoroobi@gmail.com>
2026-02-23 17:05:22 +0330

    modify formard func and add Tanh class

--- 07_RNN_Application/RNN.py
+++ 07_RNN_Application/RNN.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class RNN:
 
-    def __init__(self, X_t, n_neurons):
+    def __init__(self, X_t, n_neurons, Activation):
         self.T = max(X_t.shape)
         self.X_t = X_t
         # inizializing prediction vector of yt
@@ -17,10 +17,47 @@ class RNN:
         self.biases = 0.1 * np.random.randn(n_neurons, 1)
 
         self.H = [np.zeros((self.n_neurons, 1)) for t in range(self.T + 1)]
+        self.Activation = Activation
 
-    def forward(self, xt, ht_1):
-        out = np.dot(self.Wx, xt) + np.dot(self.Wh, ht_1) + self.biases
-        ht = np.tanh(out)
-        y_hat_t = np.dot(self.Wy, ht)
+    # def forward(self, xt, ht_1):
+    def forward(self):
+        self.dWx = np.zeros((self.n_neurons, 1))
+        self.dWh = np.zeros((self.n_neurons, self.n_neurons))
+        self.dWy = np.zeros((1, self.n_neurons))
+        self.dbiases = np.zeros((self.n_neurons, 1))
 
-        return ht, y_hat_t, out
+        X_t = self.X_t
+        H = self.H
+        Y_hat = self.Y_hat
+        ht = H[0]
+
+        Activation = self.Activation
+
+        ACT = [Activation for t in range(self.T)]
+
+        [ACT, H, Y_hat] = self.RNNCell(X_t, ht, ACT, H, Y_hat)
+
+    def RNNCell(self, X_t, ht, ACT, H, Y_hat):
+        for t, xt in enumerate(X_t):
+            xt = xt.reshape(1, 1)
+            out = np.dot(self.Wx, xt) + np.dot(self.Wh, ht) + self.biases
+            ACT[t].forward(out)
+            ht = ACT[t].output
+            # ht = np.tanh(out)
+            y_hat_t = np.dot(self.Wy, ht)
+            H[t + 1] = ht
+            Y_hat[t] = y_hat_t
+
+        # return ht, y_hat_t, out
+        return (ACT, H, Y_hat)
+
+
+class Tanh:
+
+    def forward(self, inputs):
+        self.output = np.tanh(inputs)
+        self.inputs = inputs
+
+    def backward(self, dvalues):
+        deriv = 1 - self.output ** 2
+        self.dinputs = np.multiply(deriv, dvalues)

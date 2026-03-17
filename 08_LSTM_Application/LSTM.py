902ebab658e83b9da30e5cd5e8440031262b9f43
mohamadghoroobi <m.ghoroobi@gmail.com>
2026-03-17 19:25:09 +0330

    Layer_Dense class finished.

--- 08_LSTM_Application/LSTM.py
+++ 08_LSTM_Application/LSTM.py
@@ -204,8 +204,6 @@ class LSTM:
         self.H = H
 
 
-
-
 class Sigmoid:
 
     def forward(self, M):
@@ -253,3 +251,24 @@ class Tanh:
         #       = (y-y_hat) * (1-tanh^2)
         #       = dvalues   * deriv
         self.dinputs = np.multiply(deriv, dvalues)
+
+
+class Layer_Dense():
+
+    def __init__(self, n_inputs, n_neurons):
+        # note: we are using randn here in order to see if neg values are
+        # clipped by the ReLU
+        # import numpy as np
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    # passing on the dot product as input for the next layer, as before
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs  # we're gonna need for backprop
+
+    def backward(self, dvalues):
+        # gradients
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
